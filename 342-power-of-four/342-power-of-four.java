36b6ce7b513eeb6c5a8fd74d6c1bc6df14d86c63
nebi <45132902+nebi19@users.noreply.github.com>
2022-03-20 05:58:14 -0700

    Time: 3 ms (13.20%), Space: 41.3 MB (47.79%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+         int count0=0;
+    int count1=0;
+ 
+    while(n>0){
+        if((n&1)==1){
+            count1++;
+        }else{
+            count0++;
+        }
+ 
+        n>>=1;
+    }
+ 
+    return count1==1 && (count0%2==0);
+        
+    }
+}
