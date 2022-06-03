740cf946b2c43d74eee2d25f4cf25806443dfa7d
nebi <45132902+nebi19@users.noreply.github.com>
2022-06-03 10:55:15 -0700

    Time: 13 ms (50.23%), Space: 71.2 MB (22.91%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,10 +3,11 @@ class Solution {
         HashMap<Integer,Integer> fre=new HashMap<Integer,Integer>();
         boolean flag=false;
         for(int i:nums){
+            if(fre.containsKey(i)) return true;
           int  occ=fre.getOrDefault(i,0)+1;
-            if(occ>=2){
-                flag=true;
-            }
+          //   if(occ>=2){
+          //       flag=true;
+          //   }
             fre.put(i,occ);
         }
         return flag;
