fb21009af358f6a4eb0c6c7e0f998543df44fe75
nebi <45132902+nebi19@users.noreply.github.com>
2022-06-04 21:12:01 -0700

    Time: 3 ms (41.11%), Space: 42.9 MB (5.76%) - LeetHub

--- 274-h-index/274-h-index.java
+++ 274-h-index/274-h-index.java
@@ -1,14 +1,23 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int n=citations.length;
-        for(int i=0;i<citations.length;i++){
-            if(citations[i]>=n){
-                return n;
+Arrays.sort(citations);
+        int low = 0;
+        int high = citations.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            final int numberOfCitationsGreaterThanOrEqualToCurrentMidElement = citations.length - mid; // array is ascending order. .
+            // so the number of elements after the mid elements is the number of citations greater than equal to the mid Element.
+            if (citations[mid] == numberOfCitationsGreaterThanOrEqualToCurrentMidElement)
+                return citations[mid];
+            else if (citations[mid] < numberOfCitationsGreaterThanOrEqualToCurrentMidElement) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            n--;
         }
-        return 0;
+        return citations.length - low;
         
     }
 }
