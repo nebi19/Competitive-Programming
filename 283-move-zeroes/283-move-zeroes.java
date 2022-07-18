500b40f4fabe14673d6fce390dd9ded7f2e59df4
nebi <45132902+nebi19@users.noreply.github.com>
2022-07-18 07:20:02 -0700

    Time: 4 ms (18.51%), Space: 54.5 MB (65.36%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,15 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        int j=0;
-        while(i<nums.length){
-            if(nums[i]!=0){
-                nums[j++]=nums[i];
-            }
-        i++;
-        }
-        while(j<nums.length){
-            nums[j++]=0;
+  int j=0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]!=0) {
+                nums[j] = nums[i];
+                // Change element at current pointer only if pointers do not collide
+                if (i!=j) nums[i] = 0;
+                j++;
+            } 
         }
         
     }
