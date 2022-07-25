e3a59d12429bb399407c4457eedb73184f708b7a
nebi <45132902+nebi19@users.noreply.github.com>
2022-07-25 23:14:40 +0300

    Time: 3 ms (32.31%), Space: 48.4 MB (40.34%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -1,31 +1,19 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-                 List<String> list = new ArrayList<>();
-                
-                HashMap<Integer,String> dict = new HashMap<>(){{
-                        put(3,"Fizz");
-                        put(5,"Buzz");
-                }};// mapping to store fizzbuzz mappings
-                
-                List<Integer> divisors = new ArrayList<>(Arrays.asList(3,5));
-            int i=1;
-            while(i<=n){
-                    String str = "";
-                    
-                    for(Integer key : divisors){
-                       if(i%key == 0){
-                               str += dict.get(key);
-                       }     
-                    }
-                    
-                    if(str.equals("")) str += Integer.toString(i);
-                    
-                    list.add(str);
-                    
-                    i++;
-            }
-           
-        return list;
+        List<String> rs=new ArrayList<>();
+       for(int i=1;i<=n;i++){
+           if(i%3==0&&i%5==0){
+               rs.add("FizzBuzz");
+           }else if(i%3==0){
+               rs.add("Fizz");
+           }
+           else if(i%5==0){
+               rs.add("Buzz");
+           }else{
+               rs.add(i+"");
+           }
+       }
+        return rs;
         
     }
 }
