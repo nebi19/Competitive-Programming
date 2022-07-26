25b82ae10bdbe761c63382dbcc0f88d6fc18c3fa
nebi <45132902+nebi19@users.noreply.github.com>
2022-07-26 16:04:26 +0300

    Time: 0 ms (100.00%), Space: 40.1 MB (95.65%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,21 +1,14 @@
 class Solution {
     public String sortSentence(String s) {
-        String[] arr = s.split(" ");
-        Arrays.sort(arr, new Comparator<String>() {
-           public int compare(String s1, String s2) {
-               int ch1 = s1.charAt(s1.length()-1);
-               int ch2 = s2.charAt(s2.length()-1);
-               if(ch1 <= ch2) {
-                   return -1;
-               } else return 1;
-           } 
-        });
-        StringBuilder str = new StringBuilder();
-        for(String st : arr) {
-            String temp = st.substring(0, st.length()-1);
-            str.append(temp + " ");
+        s=s.trim();
+        String[] answer=new String[s.split(" ").length];
+        String[] word=s.split(" ");
+        for(String sa:word){
+            int position=sa.charAt(sa.length()-1)-'0';
+            answer[position-1]=sa.substring(0,sa.length()-1);
         }
-        return str.toString().trim();
+        
+        return String.join(" ",answer);
         
     }
 }
