558cbb7a1f72945ceaa6d954bfda241b7a71bb60
nebi <45132902+nebi19@users.noreply.github.com>
2022-07-26 16:26:26 +0300

    Time: 1 ms (99.59%), Space: 44.1 MB (31.53%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,12 +1,23 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer> rs=new ArrayList<>();
-         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target) rs.add(i);
-        }
-        return rs;
-       
+        // List<Integer> rs=new ArrayList<>();
+        //  Arrays.sort(nums);
+        //  List<Integer> rs=search(nums,0,nums.length-1);
+        // return rs;
+          List<Integer> list = new ArrayList<>();
+        int targetFreq = 0, lessThanTarget = 0;
         
+        for(int num : nums){
+            if(num == target)
+                targetFreq++;
+            if(num < target)
+                lessThanTarget++;
+        }
+        
+        for(int i = lessThanTarget; i < (lessThanTarget + targetFreq); i++){
+            list.add(i);
+        }
+        return list;
     }
+   
 }
