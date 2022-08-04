a44df75976b4f607790dde46db6a2ff22a928fb3
nebi <45132902+nebi19@users.noreply.github.com>
2022-08-04 07:15:28 +0300

    Time: 60 ms (46.91%), Space: 178.7 MB (5.15%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,19 +1,28 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        boolean change=true;
-        do{
-            change=false;
-              for(int i=1;i<nums.length-1;i++){
-            float a=nums[i-1]+nums[i+1];
-            if(a/2==nums[i]){
-                int b=nums[i];
-                nums[i]=nums[i+1];
-                nums[i+1]=b;
-                change=true;
-            }
+        Arrays.sort(nums);
+        // for(int i=0;i<=nums.length/2;i+=2){
+        //     int temp=nums[i];
+        //     nums[i]=nums[nums.length-1-i];
+        //     nums[nums.length-1-i]=temp;              
+        // }
+        //12345
+        //i=0 52341
+        //i=2 52 
+        int i=0;
+        int start=0;
+        int end=nums.length-1;
+        int[] rs=new int[nums.length];
+        while(end>start){
+            rs[i]=nums[start];
+            rs[++i]=nums[end];
+            i++;
+            start++;
+            end--;
+            
         }
-        }while(change);
-      
-        return nums;
+        if(nums.length%2!=0) rs[i]=nums[end];
+        return rs;
+        
     }
 }
