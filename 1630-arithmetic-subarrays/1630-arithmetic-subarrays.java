d54727a0be6bd2bd138f298791fb07867768bd81
nebi <45132902+nebi19@users.noreply.github.com>
2022-08-10 06:44:51 +0300

    Time: 19 ms (93.28%), Space: 43.4 MB (86.29%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,22 +1,40 @@
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-      List<Boolean> ans=new ArrayList();
-        for(int i=0;i<l.length;i++){
-             Boolean item=isArthemetic(Arrays.copyOfRange(nums, l[i], r[i]+1));  
-             ans.add(item);
-        }
-        return ans;
-    }
-    Boolean isArthemetic(int[] nums){
-         Arrays.sort(nums);
-            int len = nums.length;
-            int d = nums[1] - nums[0];
-            for (int i = 2; i <= len - 1; i++) {
-                if (nums[i] - nums[i - 1] != d) {
-                    return false;
-                }
-            }
-            return true;
+           List<Boolean> list = new ArrayList<>() ;
         
+        for(int i=0 ; i<l.length ; i++){
+            int[] array = creatingArray(l[i] , r[i] , nums) ;
+            if(checkArrayArithmetic(array)==true ){
+                list.add(true) ;
+            }else{
+                list.add(false) ;
+            } 
+        }
+        return list ;
     }
+    
+    public int[] creatingArray(int i , int j , int[] nums){
+         int[] res= new int[j-i+1] ;
+         int i1=0 ;
+        for(int k=i ; k<=j ; k++){
+            res[i1]=nums[k] ;
+            i1++ ;
+        }
+        
+        return res ;
+    }
+    
+    public boolean checkArrayArithmetic(int[] array){
+        Arrays.sort(array) ;
+        int diff=array[1]-array[0] ;      
+        for(int i=0 ; i<array.length-1 ; i++){
+            if(array[i+1]-array[i]!=diff){
+                return false ;
+            }
+        } 
+        
+        return true ;
+    }
+        
+    
 }
