7be794b8cc0b9dab3a3ffcd654ed3eec01d82b90
nebi <45132902+nebi19@users.noreply.github.com>
2022-08-12 07:37:25 +0300

    Time: 6 ms (55.89%), Space: 42.8 MB (29.70%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,24 +1,27 @@
 class Solution {
     public String reverseParentheses(String s) {
-        Stack<Character> st=new Stack();
-        Queue<Character> q= new LinkedList<>();
-        for(char c:s.toCharArray()){
-            if(c==')'){
-               while(!(st.peek()=='('||st.empty())){
+        Stack<Character> st=new Stack<>();
+         Queue<Character> q = new LinkedList();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')'){
+                while(st.peek() != '('){
                     q.add(st.pop());
-               }
-                st.pop();
-                while(!q.isEmpty()){
-                   st.push(q.poll()); 
                 }
-                
-            }else{
-                st.push(c);
+                st.pop();
+            while(q.size() > 0){
+                st.push(q.remove());
             }
-         }
-     	char[] ans = new char[st.size()];
-    	for(int i = ans.length - 1; i >= 0; i--)
-    		ans[i] = st.pop();
-    	return new String(ans);
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+       char ans[] = new char[st.size()];
+        int i = ans.length-1;
+        while(i >= 0){
+            ans[i] = st.pop();
+            i--;
+        }
+        return new String(ans);
+        
     }
 }
