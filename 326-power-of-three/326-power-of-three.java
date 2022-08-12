95614b87fc8ecc7b4e13bc8dfe86435115f85b6a
nebi <45132902+nebi19@users.noreply.github.com>
2022-08-12 07:41:19 +0300

    Time: 28 ms (21.31%), Space: 46.8 MB (80.27%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        //  if(n==1) return true;
-        // if(n%3!=0) return false;
-        // if(n==0) return false;
-        // return isPowerOfThree(n/3);
-        return Math.log10(n)/Math.log10(3)%1==0;
+          if(n==0) return false;
+ 
+    return n == Math.pow(3, Math.round(Math.log(n)/Math.log(3)));
         
     }
 }
