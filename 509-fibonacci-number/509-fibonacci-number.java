9664fc85d2f2e49b1ff6aab2e5f4d5435ac21f29
nebi <45132902+nebi19@users.noreply.github.com>
2022-09-25 12:32:47 +0300

    Time: 0 ms (100.00%), Space: 39.1 MB (92.42%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,12 @@
 class Solution {
     public int fib(int n) {
-        if(n<2) return n;
-        return fib(n-1)+fib(n-2);
-        
+        int[] memo=new int[n+1];
+        return calFib(n,memo);        
+    }
+    int calFib(int n,int[] memo){
+      if(n<2) return n;
+        if(memo[n]!=0)return memo[n];
+        memo[n]=calFib(n-1,memo)+calFib(n-2,memo);
+        return memo[n];
     }
 }
